77dd3808e5e006f22622f5e74dddbf1cb582e164
azzamzn <azzam.zaidan00@gmail.com>
2021-10-27 21:26:34 +0700

    add new donation

---
+++ src/main/java/com/spboot/projectduagroup6/controllers/DonationController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spboot.projectduagroup6.controllers;
+
+import com.spboot.projectduagroup6.interfaces.DonationInterface;
+import com.spboot.projectduagroup6.models.Donation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author Dell
+ */
+
+@Controller
+public class DonationController {
+
+    @Autowired
+    private DonationInterface donationInterface;
+
+    @GetMapping("/donation")
+    public String index(Model model) {
+        model.addAttribute("list", donationInterface.getAll());
+        return "donation";
+    }
+    
+
+
+    @GetMapping("/adddonation")
+    public String create(Model model) {
+        
+        Donation donation = new Donation();
+        model.addAttribute("donation", donation);
+        
+        return "adddonation";
+    }
+
+    @PostMapping("/adddonation/store")
+    public String store(@ModelAttribute("adddonation") Donation donation) {
+        donationInterface.store(donation);
+        return "redirect:/";
+    }
+}
+
